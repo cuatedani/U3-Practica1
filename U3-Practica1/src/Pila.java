5cee1903b321a7a5619f0eb55d0067e576165447
cuatedani <72040210+cuatedani@users.noreply.github.com>
2020-10-24 23:35:59 -0600

    Add files via upload

---
+++ U3-Practica1/src/Pila.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER}
+ */
+public class Pila {
+    private int P[];
+    private int Indice;
+    
+    public Pila(int v){
+        P=new int[v];
+        Indice=-1;
+    }
+    
+    public boolean INSERTAR(int v){
+        if(Indice==(P.length-1)){
+            return false;
+        }
+        Indice++;
+        P[Indice]=v;
+        return true;
+    }
+    
+    public boolean ELIMINAR(){
+        if(Indice!=-1){
+            P[Indice]=0;
+            Indice--;
+            return true;
+        }
+        return false;
+    }       
+}
